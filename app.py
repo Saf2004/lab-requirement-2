7d3bcf0c65d78bcc054aff6e89fd2b3cdb365cc9
safey <s-safey.yahia@zewailcity.edu.eg>
2024-10-09 09:32:24 +0300

    add flask backend

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/form', methods=['GET', 'POST'])
-def form():  # put application's code here
+@app.route('/', methods=['GET', 'POST'])
+def form():
     if request.method == 'POST':
-        return 'Submitted'
+        name = request.form.get('Name')
+        email = request.form.get('email')
+        return render_template('greetings.html', name=name, email=email)
+    else:
+        return render_template('index.html')
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
